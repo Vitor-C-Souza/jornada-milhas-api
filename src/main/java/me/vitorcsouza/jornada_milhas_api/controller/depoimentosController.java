caeef88d34b4bc794344f4325f25f71b217d2980
Vitor-C-Souza <vitor_souza97@hotmail.com>
2024-09-16 10:12:06 -0300

    Ajustado o spring doc para ficar mais facil de ser entendido pelos usuarios

--- src/main/java/me/vitorcsouza/jornada_milhas_api/controller/depoimentosController.java
+++ src/main/java/me/vitorcsouza/jornada_milhas_api/controller/depoimentosController.java
@@ -1,9 +1,11 @@
 package me.vitorcsouza.jornada_milhas_api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import me.vitorcsouza.jornada_milhas_api.domain.dto.depoimentosDtoReq;
 import me.vitorcsouza.jornada_milhas_api.domain.dto.depoimentosDtoRes;
 import me.vitorcsouza.jornada_milhas_api.domain.service.depoimentosService;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -21,6 +23,7 @@ public class depoimentosController {
     private depoimentosService service;
 
     @PostMapping("/depoimentos")
+    @Operation(summary = "Cria um novo depoimento", tags = {"depoimentos"})
     public ResponseEntity<depoimentosDtoRes> create(@RequestBody @Valid depoimentosDtoReq dto, UriComponentsBuilder uri) {
         depoimentosDtoRes dtoRes = service.create(dto);
         URI endereco = uri.path("depoimentos/{id}").buildAndExpand(dtoRes.id()).toUri();
@@ -28,28 +31,33 @@ public class depoimentosController {
     }
 
     @GetMapping("/depoimentos/{id}")
+    @Operation(summary = "Procura um depoimento especifico pelo seu id", tags = {"depoimentos"})
     public ResponseEntity<depoimentosDtoRes> findById(@PathVariable Long id) {
         return ResponseEntity.ok(service.findById(id));
     }
 
     @GetMapping("/depoimentos")
-    public ResponseEntity<Page<depoimentosDtoRes>> findAll(@PageableDefault Pageable pag) {
+    @Operation(summary = "Lista todo os depoimentos salvos", tags = {"depoimentos"})
+    public ResponseEntity<Page<depoimentosDtoRes>> findAll(@PageableDefault @ParameterObject Pageable pag) {
         return ResponseEntity.ok(service.findAll(pag));
     }
 
     @PutMapping("/depoimentos/{id}")
+    @Operation(summary = "Atualiza um depoimento", tags = {"depoimentos"})
     public ResponseEntity<depoimentosDtoRes> update(@PathVariable Long id, @RequestBody @Valid depoimentosDtoReq dto) {
         return ResponseEntity.ok(service.update(id, dto));
     }
 
     @DeleteMapping("/depoimentos/{id}")
+    @Operation(summary = "Deleta um depoimento salvo", tags = {"depoimentos"})
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/depoimentos-home")
-    public ResponseEntity<Page<depoimentosDtoRes>> findRandom(@PageableDefault(size = 3) Pageable pag) {
+    @Operation(summary = "Retorna 3 depoimentos aleatoriamente", tags = {"depoimentos"})
+    public ResponseEntity<Page<depoimentosDtoRes>> findRandom(@PageableDefault(size = 3) @ParameterObject Pageable pag) {
         return ResponseEntity.ok(service.findRandom(pag));
     }
 }
--- src/main/java/me/vitorcsouza/jornada_milhas_api/controller/destinoController.java
+++ src/main/java/me/vitorcsouza/jornada_milhas_api/controller/destinoController.java
@@ -1,10 +1,12 @@
 package me.vitorcsouza.jornada_milhas_api.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import me.vitorcsouza.jornada_milhas_api.domain.dto.destinoDtoReq;
 import me.vitorcsouza.jornada_milhas_api.domain.dto.destinoDtoRes;
 import me.vitorcsouza.jornada_milhas_api.domain.dto.destinoDtoResWParamGet;
 import me.vitorcsouza.jornada_milhas_api.domain.service.destinoService;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +24,7 @@ public class destinoController {
     private destinoService service;
 
     @PostMapping
+    @Operation(summary = "Cria um novo destino", tags = {"destinos"})
     public ResponseEntity<destinoDtoRes> createDestino(@RequestBody @Valid destinoDtoReq dto, UriComponentsBuilder uri) {
         destinoDtoRes dtoRes = service.create(dto);
         URI endereco = uri.path("/destinos/{id}").buildAndExpand(dtoRes.id()).toUri();
@@ -29,7 +32,8 @@ public class destinoController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<destinoDtoRes>> findAll(@RequestParam(required = false) String nome, @PageableDefault Pageable pageable) {
+    @Operation(summary = "Lista todos os destinos ou aqueles que enviar como parametro na requisição como busca por destinos especificos", tags = {"destinos"})
+    public ResponseEntity<Page<destinoDtoRes>> findAll(@RequestParam(required = false) String nome, @PageableDefault @ParameterObject Pageable pageable) {
         Page<destinoDtoRes> dtoResPage;
         if (nome == null) {
             dtoResPage = service.findAll(pageable);
@@ -40,18 +44,21 @@ public class destinoController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Procura um destino especifico pelo seu id", tags = {"destinos"})
     public ResponseEntity<destinoDtoResWParamGet> findById(@PathVariable Long id) {
         destinoDtoResWParamGet dtoRes = service.findById(id);
         return ResponseEntity.ok(dtoRes);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualiza um destino", tags = {"destinos"})
     public ResponseEntity<destinoDtoRes> update(@PathVariable Long id, @RequestBody destinoDtoReq dto) {
         destinoDtoRes dtoRes = service.update(id, dto);
         return ResponseEntity.ok(dtoRes);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Apaga um destino salvo pelo seu id", tags = {"destinos"})
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
